d3c62e194601a2a8247c6e47c4c57d6fb2c0ddce
lab_roso_stud15 <lab_roso_stud15@1cff1db6-d32c-402b-8377-92532def4f20>
2015-05-30 13:46:19 +0000

    actualized version of part 2 collision avoidance

--- SampleCode/src/soccer_sample.cpp
+++ SampleCode/src/soccer_sample.cpp
@@ -68,18 +68,9 @@ int main(void) {
 		RoboControl robo(DBC, rfcomm_nr);
 
 		/** Now let's print out some information about the robot... */
-                //uint8_t mac[6];
-                //robo.GetMac(mac);
-                cout << "Robo @ rfcomm" << rfcomm_nr << endl; /*<<" with Mac: ";
 
-
-		for (int j = 0; j < 5; j++)
-			cout << hex << (int) mac[j] << ":";
-		cout << hex << (int) mac[5] << endl;
-*/
-                cout << "\t Battery Voltage: " << dec << (int) robo.GetAccuVoltage()
-                                << "mV" << endl;
-		cout << "\t initial position: " << robo.GetPos() << endl;
+                cout << "Robo @ rfcomm" << rfcomm_nr << endl;
+                cout << "\t initial position: " << robo.GetPos() << endl;
 		cout << "\t initial rotation: " << robo.GetPhi() << endl;
 
 		/** Create a ball object
@@ -100,98 +91,112 @@ int main(void) {
 
 		//-------------------------------------- Ende Init ---------------------------------
 
-		/** Define four positions which form a rectangle...
-		 *
-		 */
-		Position pos1(-0.6, -0.6);
-		Position pos2(0.6, -0.6);
-		Position pos3(0.6, 0.6);
-		Position pos4(-0.6, 0.6);
+
                 double a;
                 double b;
                 double x1;
                 double x2;
+                cout <<  "initial ball pos " << ball.GetPos() << endl ;
+                while (1)
+                {
 
-		while (1) {
-			/** Sequentially move to the four different positions.
-                         *  The while is excited if the position is reached.
+                    bool test= (fabs(ball.GetX())<1.4) && (fabs(ball.GetY())<0.83 )&& !((fabs(ball.GetX())>1.14) && (fabs(ball.GetY())<0.4));
 
+                    if (test)
 
-			cout << "Moving to " << pos1 << endl << endl;
-                        robo.GotoXY(pos1.GetX(), pos1.GetY(), 160, false);
-                        while (robo.GetPos().DistanceTo(pos1) > 0.10) usleep(50000); //sleep function in microseconds
-                        //Camera sampling rate is 30fps -> 33ms
-                        //which means that field info does not change within this time
-
-
-			cout << "Moving to " << pos2 << endl << endl;
-                        robo.GotoXY(pos2.GetX(), pos2.GetY(), 160, true);
-                        while (robo.GetPos().DistanceTo(pos2) > 0.10) usleep(50000);
-
-
-			cout << "Moving to " << pos3 << endl << endl;
-                        robo.GotoXY(pos3.GetX(), pos3.GetY(), 160, false);
-                        while (robo.GetPos().DistanceTo(pos3) > 0.10) usleep(50000);
-
-
-			cout << "Moving to " << pos4 << endl << endl;
-                        robo.GotoXY(pos4.GetX(), pos4.GetY(), 160, true);
-                        while (robo.GetPos().DistanceTo(pos4) > 0.10) usleep(50000);*/
-
-
-                    if ((fabs(ball.GetX())<1.3) && (fabs(ball.GetY())<0.75 )&& !((fabs(ball.GetX())>1.14) && (fabs(ball.GetY())<0.4)))
-
-{ // cout <<  "Moving to " << ball.GetPos() << endl ;
+                    {
+                        cout <<  "Moving to " << ball.GetPos() << endl ;
                         a = (ball.GetY()-robo.GetY())/(ball.GetX()-robo.GetX());
                         b=ball.GetY()-a*ball.GetX();
                         x1= (0.42 -b)/a;
                         x2= (-0.34-b)/a;
-                        if (((fabs(x1)>1.14) && (fabs(x1)<1.35)) || ((fabs(x2)>1.14)&& (fabs(x2)<1.35)))
-                        { if (robo.GetX()>0)
-                              { Position pos5(1.0, robo.GetY());
+                        if (((fabs(x1)>1.14) && (fabs(x1)<1.4)) || ((fabs(x2)>1.14)&& (fabs(x2)<1.4)))
+                        {
+                            cout <<  "intersection " << ball.GetPos() << endl ;
+                            if ((robo.GetX()>0) && (ball.GetX()>0)&& ((fabs(ball.GetX())<1.4) && (fabs(ball.GetY())<0.83 )&& !((fabs(ball.GetX())>1.14) && (fabs(ball.GetY())<0.4))))
+                            {
+                                cout << "same positive signs" << robo.GetPos()<< "    " << ball.GetPos() << endl ;
+                                Position pos5(1.0, robo.GetY());
+                                cout << "step1" << endl;
                                 robo.GotoXY(pos5.GetX(),pos5.GetY());
-                             while (robo.GetPos().DistanceTo(pos5) > 0.10) usleep(50000);
-                           Position pos6(1.0, ball.GetY());
-                              robo.GotoXY(pos6.GetX(),pos6.GetY());
-                             while (robo.GetPos().DistanceTo(pos6) > 0.10) usleep(50000);
-                             robo.GotoXY(ball.GetX(),ball.GetY(),160,true);
-                             //while (robo.GetPos().DistanceTo(ball.GetPos()) > 0.10) usleep(50000);
+                                while (robo.GetPos().DistanceTo(pos5) > 0.10 && ((fabs(ball.GetX())<1.4) && (fabs(ball.GetY())<0.83 )&& !((fabs(ball.GetX())>1.14) && (fabs(ball.GetY())<0.4))))
+                                    usleep(50000);
+                                cout << "end step1" << endl;
+                                Position pos6(1.0, ball.GetY());
+                                cout << "step2" << endl;
+                                robo.GotoXY(pos6.GetX(),pos6.GetY());
+                                while (robo.GetPos().DistanceTo(pos6) > 0.10 &&((fabs(ball.GetX())<1.4) && (fabs(ball.GetY())<0.83 )&& !((fabs(ball.GetX())>1.14) && (fabs(ball.GetY())<0.4))))
+                                    usleep(50000);
+                                cout << "end step2" << endl;
+                                robo.GotoXY(ball.GetX(),ball.GetY(),160,true);
+                             //while (robo.GetPos().DistanceTo(ball.GetPos()) > 0.10 qnd test) usleep(50000);
                             }
-                            else {Position pos5(-1.0, robo.GetY());
+                            else if ((robo.GetX()<0) && (ball.GetX()<0) && ((fabs(ball.GetX())<1.4) && (fabs(ball.GetY())<0.83 )&& !((fabs(ball.GetX())>1.14) && (fabs(ball.GetY())<0.4))))
+                            {
+                                cout << "same negative signs" << robo.GetPos()<< "    " << ball.GetPos() << endl ;
+                                Position pos5(-1.0, robo.GetY());
                                 robo.GotoXY(pos5.GetX(),pos5.GetY());
-                             while (robo.GetPos().DistanceTo(pos5) > 0.10) usleep(50000);
-                           Position pos6(-1.0, ball.GetY());
-                              robo.GotoXY(pos6.GetX(),pos6.GetY());
-                             while (robo.GetPos().DistanceTo(pos6) > 0.10) usleep(50000);
-                             robo.GotoXY(ball.GetX(),ball.GetY(),160,true);}
+                                while (robo.GetPos().DistanceTo(pos5) > 0.10 && ((fabs(ball.GetX())<1.4) && (fabs(ball.GetY())<0.83 )&& !((fabs(ball.GetX())>1.14) && (fabs(ball.GetY())<0.4))))
+                                    usleep(50000);
+                                Position pos6(-1.0, ball.GetY());
+                                robo.GotoXY(pos6.GetX(),pos6.GetY());
+                                while (robo.GetPos().DistanceTo(pos6) > 0.10 && (fabs(ball.GetX())<1.4) && (fabs(ball.GetY())<0.83 )&& !((fabs(ball.GetX())>1.14) && (fabs(ball.GetY())<0.4)))
+                                    usleep(50000);
+                                robo.GotoXY(ball.GetX(),ball.GetY(),160,true);
+                            }
+                            else
+                            {
+                                cout << "different signs" << robo.GetPos()<< "    " << ball.GetPos() << endl ;
+                                if ((fabs(ball.GetX())<1.4) && (fabs(ball.GetY())<0.83 )&& !((fabs(ball.GetX())>1.14) && (fabs(ball.GetY())<0.4)))
+                                {
+                                    Position pos5(0.0, robo.GetY());
+                                    robo.GotoXY(pos5.GetX(),pos5.GetY());
+                                    while (robo.GetPos().DistanceTo(pos5) > 0.10 && ((fabs(ball.GetX())<1.4) && (fabs(ball.GetY())<0.83 )&& !((fabs(ball.GetX())>1.14) && (fabs(ball.GetY())<0.4))))
+                                        usleep(50000);
+                                    Position pos6(0.0, ball.GetY());
+                                    robo.GotoXY(pos6.GetX(),pos6.GetY());
+                                    while (robo.GetPos().DistanceTo(pos6) > 0.10 && ((fabs(ball.GetX())<1.4) && (fabs(ball.GetY())<0.83 )&& !((fabs(ball.GetX())>1.14) && (fabs(ball.GetY())<0.4))))
+                                        usleep(50000);
+                                    robo.GotoXY(ball.GetX(),ball.GetY(),160,true);
+                                }
+                            }
 
                         }
-                       /** else if (((fabs(x1)>1.14) && (fabs(x1)<1.35)) && !((fabs(x2)>1.14)&& (fabs(x2)<1.35))) {
-                            //if (robo.GetY()<0.4)
-                             Position pos7 (0.0,0.6);
-                                robo.GotoXY(pos7.GetX(),pos7.GetY());
-                            while (robo.GetPos().DistanceTo(pos7) > 0.10) usleep(50000);
-                            robo.GotoXY(ball.GetX(),ball.GetY(),160,true);
-                            //else { Position pos8 (1.0,0.5);
-
-                                cout << "zes" << robo.GetPos() << "pos ball" << ball.GetPos()<<endl << endl;
-                               // robo.GotoXY(pos8.GetX(),pos8.GetY());
-                           //while (robo.GetPos().DistanceTo(pos8) > 0.10) usleep(50000);
-                            //robo.GotoXY(ball.GetX(),ball.GetY(),160,true);}
-
-                        }*/
-
                         else
-                        robo.GotoXY(ball.GetX(),ball.GetY(),160,true);}
-                   // while (robo.GetPos().DistanceTo(ball.GetPos()) > 0.10) usleep(50000);
+                        {
+                            cout <<  "pas dintersectionin " << ball.GetPos() << endl ;
+                            if ((fabs(ball.GetX())<1.4) && (fabs(ball.GetY())<0.83 )&& !((fabs(ball.GetX())>1.14) && (fabs(ball.GetY())<0.4)))
+                            {
+                                robo.GotoXY(ball.GetX(),ball.GetY(),160,true);
+                                //while (robo.GetPos().DistanceTo(ball.GetPos()) > 0.10 && (((fabs(ball.GetX())<1.3) && (fabs(ball.GetY())<0.75 )&& !((fabs(ball.GetX())>1.14) && (fabs(ball.GetY())<0.4)))) )
+                                //{
+                                   // usleep(30000);
+
+
+
+                                //}
+
+                            }
+                        }
+
+                    }
+                    else
+
+                        cout <<"ball in forbidden region " << ball.GetPos() << endl;
 
 		}
 
-	} catch (DBError err) {
+        }
+
+
+        catch (DBError err)
+        {
 		cout << "Client died on Error: " << err.what() << endl;
 	}
+
         cout << "End" << endl;
 	return 0;
+
 }
 
 
