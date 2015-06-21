f7db96d82012e39348ce792920d43a6d92a5e37d
lab_roso_stud15 <lab_roso_stud15@1cff1db6-d32c-402b-8377-92532def4f20>
2015-06-21 12:13:06 +0000

    update the matrix update function

--- Project/src/matrixupdate.cpp
+++ Project/src/matrixupdate.cpp
@@ -6,13 +6,13 @@
 #include "referee.h"
 using namespace std;
 // function that updates the matrix
- int** matrixupdate(int h,int w, int** matrix, RoboControl *robots[6], RawBall *ball, CoordinatesCalibrer *coordCalibrer, eSide our_side);
+ int** matrixupdate(int h,int w, int** matrix, RoboControl *robots[6], RawBall ball, CoordinatesCalibrer *coordCalibrer, eSide our_side);
  int main()
  {   const int client_nr = 15;
      int rfcomm_nr_blue[] = {0, 1, 2};
      int rfcomm_nr_red[] = {3, 4, 5};
 
-     int *rfcomm_nr = team == BLUE_TEAM ? rfcomm_nr_blue : rfcomm_nr_red;
+    // int *rfcomm_nr = team == BLUE_TEAM ? rfcomm_nr_blue : rfcomm_nr_red;
      //int *rfcomm_nr_2 = team == RED_TEAM ? rfcomm_nr_blue : rfcomm_nr_red;
 
      try
@@ -21,21 +21,36 @@ using namespace std;
          string client_name = "pololu_client_";
          client_name.push_back((char)(client_nr + '0'));
          RTDBConn DBC(client_name.data(), 0.1, "");
-    int initialMatrix[5][5];
+         int **Matrix = new int*[5];
+        for (int i = 0; i <5; i++)
+        {
+            Matrix[i] = new int[5];
+            for (int j=0; j<5; j++)
+            {
+                Matrix[i][j]=0;
+            }
+         }
+    int** initialMatrix= Matrix;
 
-    RoboControl robo1 = RoboControl(DBC, rfcomm_nr[0]);
-    RoboControl robo2 = RoboControl(DBC, rfcomm_nr[1]);
-    RoboControl robo3 = RoboControl(DBC, rfcomm_nr[2]);
-    RoboControl robo4 = RoboControl(DBC, rfcomm_nr_2[0]);
-    RoboControl robo5 = RoboControl(DBC, rfcomm_nr_2[1]);
-    RoboControl robo6 = RoboControl(DBC, rfcomm_nr_2[2]);
+    RoboControl robo1 = RoboControl(DBC, rfcomm_nr_blue[0]);
+    RoboControl robo2 = RoboControl(DBC, rfcomm_nr_blue[1]);
+    RoboControl robo3 = RoboControl(DBC, rfcomm_nr_blue[2]);
+    RoboControl robo4 = RoboControl(DBC, rfcomm_nr_red[0]);
+    RoboControl robo5 = RoboControl(DBC, rfcomm_nr_red[1]);
+    RoboControl robo6 = RoboControl(DBC, rfcomm_nr_red[2]);
     RoboControl *robots[6] = {&robo1, &robo2, &robo3, &robo4, &robo5, &robo6};
     RawBall ball(DBC);
     Referee ref(DBC);
     ref.Init();
     CoordinatesCalibrer coordCalibrer= CoordinatesCalibrer();
     eSide ourSide= LEFT_SIDE;
-    int[][]updatedMatrix= matrixupdate(5,5,initialMatrix,*robots[6],*ball,coordCalibrer,ourSide);
+    int**updatedMatrix= matrixupdate(5,5,initialMatrix,robots,ball,&coordCalibrer,ourSide);
+    cout<<"printing the matrix"<<endl;
+    for (int i = 0; i <5; i++)
+        for (int j=0; j<5; j++)
+        {
+            cout << updatedMatrix[i][j];}
+
      }
      catch (DBError err)
      {
@@ -46,18 +61,18 @@ using namespace std;
      return 0;
  }
 
-int** matrixupdate(int h,int w, int** matrix, RoboControl *robots[6], RawBall *ball, CoordinatesCalibrer *coordCalibrer, eSide our_side)
+int** matrixupdate(int h,int w, int** matrix, RoboControl *robots[6], RawBall ball, CoordinatesCalibrer *coordCalibrer, eSide our_side)
 {
- int[][]newMatrix= matrix;
- m_coordCalibrer = coordCalibrer;
+ int** newMatrix= matrix;
+ CoordinatesCalibrer *m_coordCalibrer = coordCalibrer;
  double l=2/(h-1);
  double k=2/(w-1);
  //Normalize coordinates of our robot
- Position pos = m_coordCalibrer->NormalizePosition(robots[0].GetPos());//robots[0] is our robot
+ Position pos1 = m_coordCalibrer->NormalizePosition((*robots)[0].GetPos());//robots[0] is our robot
  //indices of the robot's position in the matrix
- int i=static_cast<int>(floor(pos.GetY()/l)-floor(-1/l));
- int j=static_cast<int>(floor(pos.GetX()/k)-floor(-1/k));
- newMatrix[i][j]=2; // affect 2 to the position of our robot in the matrix
+ int i1=static_cast<int>(floor(pos1.GetY()/l)-floor(-1/l));
+ int j1=static_cast<int>(floor(pos1.GetX()/k)-floor(-1/k));
+ newMatrix[i1][j1]=2; // affect 2 to the position of our robot in the matrix
 //Normalize coordinates of the ball
  Position pos = m_coordCalibrer->NormalizePosition(ball.GetPos());
  //indices of the robot's position in the matrix
@@ -78,7 +93,7 @@ int** matrixupdate(int h,int w, int** matrix, RoboControl *robots[6], RawBall *b
  for (int i=1 ; i < 6 ; i++)
  {
     //Normalize coordinates of the obstacles
-    Position pos = m_coordCalibrer->NormalizePosition(robots[i].GetPos());//robots[i] is an obstacle
+    Position pos = m_coordCalibrer->NormalizePosition((*robots)[i].GetPos());//robots[i] is an obstacle
     //indices of the robot's position in the matrix
     int i=static_cast<int>(floor(pos.GetY()/l)-floor(-1/l));
     int j=static_cast<int>(floor(pos.GetX()/k)-floor(-1/k));
