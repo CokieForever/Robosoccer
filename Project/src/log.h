1fb48f7f141ef57c90c2df09ebc0dfb2a3168db8
lab_roso_stud11 <CokieForever@users.noreply.github.com>
2015-06-29 16:02:51 +0000

    Added logging system. (forgot files previously)

---
+++ Project/src/log.h
@@ -0,0 +1,32 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+typedef enum
+{
+    INFO, WARNING, ERROR, DEBUG
+} LogLevel;
+
+#define LOG_1_ARGS(message)                             Log_def(message, __PRETTY_FUNCTION__, __LINE__)
+#define LOG_2_ARGS(message, level)                      Log_def(message, __PRETTY_FUNCTION__, __LINE__, level)
+
+#define GET_3RD_ARG(arg1, arg2, arg3, ...)        arg3
+#define LOG_MACRO_CHOOSER(...)                          GET_3RD_ARG(__VA_ARGS__, LOG_2_ARGS, LOG_1_ARGS, )
+
+#define Log(...)                                        LOG_MACRO_CHOOSER(__VA_ARGS__)(__VA_ARGS__)
+
+
+void Log_def(std::string message, std::string funcName, int line, LogLevel level = INFO);
+
+template<typename T>
+std::string ToString(T t)
+{
+    std::ostringstream s;
+    s << t;
+    return s.str();
+}
+
+#endif // LOG_H
